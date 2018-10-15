662d4150d2ff39166fdb5f37472f26a5d7789aff
User12987 <44153099+User12987@users.noreply.github.com>
2018-10-15 12:44:30 +0700

    Update MoneyMaker.py
    deleted some testing code

--- MoneyMaker.py
+++ MoneyMaker.py
@@ -227,13 +227,3 @@ def control_question():
         print('RESULT: HERE IS ' + str(total_errors) + ' ERRORS AFTER LEARNING')
 
 control_question()
-
-# import sched, time
-# s = sched.scheduler(time.time, time.sleep)
-# def do_something(sc): 
-#     print "Doing stuff..."
-#     # do your stuff
-#     s.enter(6, 1, do_something, (sc,))
-
-# s.enter(6, 1, do_something, (s,))
-# s.run()
